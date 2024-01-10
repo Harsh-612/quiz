a3b5dad65d9312dcb1a30b3270e5361f86080e4d
Harsh-612 <hchaturvedi126@gmail.com>
2024-01-11 02:08:10 +0530

    fixed ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex items-center justify-center bg-blue-100">
-      <div className="bg-white p-8 rounded shadow-md max-w-md">
-        <h1 className="text-4xl font-extrabold mb-4">Quiz App</h1>
-        <p className="text-gray-700 mb-8">
-          Test your knowledge with our quizzes!
-        </p>
-        <Link href="/quiz">
-          <button className="bg-[#FF5261] hover:bg-[#ec4e5b] text-white py-2 px-4 rounded transition-all duration-300">
-            Get Started
-          </button>
-        </Link>
+    <main className="w-[100vw] h-[100vh] container]">
+      <div className="min-h-screen flex items-center justify-center bg-blue-100">
+        <div className="bg-white p-8 rounded shadow-md max-w-md">
+          <h1 className="text-3xl font-bold mb-4">Quiz App</h1>
+          <p className="text-gray-600 mb-8">
+            Test your knowledge with our quizzes!
+          </p>
+          <Link href="/quiz">
+            <button className="bg-[#FF5261] hover:bg-[#ec4e5b]  text-white py-2 px-4 rounded">
+              Get Started
+            </button>
+          </Link>
+        </div>
       </div>
     </main>
   );
